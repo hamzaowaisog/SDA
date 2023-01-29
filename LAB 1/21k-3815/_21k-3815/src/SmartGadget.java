088abb42896e10369f20f0dedfab2bbe926378af
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-30 02:58:02 +0500

    Lab 1

---
+++ LAB 1/21k-3815/_21k-3815/src/SmartGadget.java
@@ -0,0 +1,29 @@
+public class SmartGadget {
+    private String name;
+    private String color;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ LAB 1/21k-3815/_21k-3815/src/smartPhone.java
@@ -0,0 +1,11 @@
+public class smartPhone extends SmartGadget{
+    private boolean multiTouchScreen;
+
+    public boolean isMultiTouchScreen() {
+        return multiTouchScreen;
+    }
+
+    public void setMultiTouchScreen(boolean multiTouchScreen) {
+        this.multiTouchScreen = multiTouchScreen;
+    }
+}
