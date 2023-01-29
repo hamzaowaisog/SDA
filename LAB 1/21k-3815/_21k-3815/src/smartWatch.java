944cc6e655396f1833accd60e39de782f2def1dc
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2023-01-30 03:02:46 +0500

    Lab 1

---
+++ LAB 1/21k-3815/_21k-3815/src/smartWatch.java
@@ -0,0 +1,11 @@
+public class smartWatch extends SmartGadget{
+    private int HeartRate;
+
+    public int getHeartRate() {
+        return HeartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        HeartRate = heartRate;
+    }
+}
